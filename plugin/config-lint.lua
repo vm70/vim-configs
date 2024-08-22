02909825fece2e576edd47ec4703b12398a8ecbe
Vincent Mercator <vmercator@protonmail.com>
2024-08-21 23:31:18 -0400

    Add Selene as Lua linter; lint when reading file

--- plugin/config-lint.lua
+++ plugin/config-lint.lua
@@ -1,9 +1,10 @@
 require("lint").linters_by_ft = {
 	markdown = { "write_good" },
+	lua = { "selene" },
 	vim = { "vint" },
 }
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
 	callback = function()
 		require("lint").try_lint()
 	end,
