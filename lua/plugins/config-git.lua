43e3e2f25d563dd576af05940d63944ef1786497
Vincent Mercator <vmercator@protonmail.com>
2024-10-16 22:35:54 -0400

    Use Gitsigns instead of Gitgutter

--- lua/plugins/config-git.lua
+++ lua/plugins/config-git.lua
@@ -1,6 +1,21 @@
 return {
 	{ "tpope/vim-fugitive" },
 	{ "rbong/vim-flog" },
-	{ "airblade/vim-gitgutter" },
-	{ "tpope/vim-rhubarb" }
+	{ "tpope/vim-rhubarb" },
+	{
+		"lewis6991/gitsigns.nvim",
+		keys = {
+			{ "]h", "<cmd>Gitsigns next_hunk<CR>", desc = "Go to next hunk" },
+			{ "[h", "<cmd>Gitsigns prev_hunk<CR>", desc = "Go to previous hunk" },
+		},
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
 }
