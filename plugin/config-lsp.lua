d85f1c66b9b82420edbda7717c3189b2b1c5b62f
Vincent Mercator <vmercator@protonmail.com>
2024-10-18 19:49:53 -0400

    Rearrange Neovim LSP config

--- plugin/config-lsp.lua
+++ plugin/config-lsp.lua
@@ -1,29 +1,41 @@
-vim.cmd("call minpac#add('neovim/nvim-lspconfig')")
+vim.fn["minpac#add"]("neovim/nvim-lspconfig")
+vim.fn["minpac#add"]("ms-jpq/coq.nvim")
+vim.fn["minpac#add"]("williamboman/mason-lspconfig.nvim")
 
-require("lspconfig").golangci_lint_ls.setup({})
-require("lspconfig").gopls.setup({})
-require("lspconfig").lua_ls.setup({})
-require("lspconfig").marksman.setup({ filetypes = { "markdown", "markdown.mdx", "markdown.pandoc", "quarto" } })
-require("lspconfig").pylsp.setup({})
-require("lspconfig").ruff_lsp.setup({})
-require("lspconfig").taplo.setup({})
-require("lspconfig").vimls.setup({})
-
-require("lspconfig").ltex.setup({
-	settings = {
-		ltex = {
-			disabledRules = {
-				["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+local servers = {
+	golangci_lint_ls = {},
+	gopls = {},
+	lua_ls = {},
+	marksman = { filetypes = { "markdown", "markdown.mdx", "markdown.pandoc", "quarto" } },
+	pylsp = {},
+	taplo = {},
+	vimls = {},
+	ltex = {
+		settings = {
+			ltex = {
+				disabledRules = {
+					["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
+				},
 			},
 		},
 	},
+}
+
+local function default_setup(server_name)
+	require("lspconfig")[server_name].setup(servers[server_name])
+	require("lspconfig")[server_name].setup(require("coq").lsp_ensure_capabilities({}))
+end
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	automatic_installation = true,
+	handlers = { default_setup },
 })
 
 vim.keymap.set("n", "<leader>cC", vim.lsp.codelens.refresh)
 vim.keymap.set("n", "<leader>cC", vim.lsp.codelens.refresh)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>cc", vim.lsp.codelens.run)
-vim.keymap.set("n", "<leader>cf", require("conform").format)
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename)
 vim.keymap.set("n", "<leader>xx", vim.diagnostic.setqflist)
 vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
