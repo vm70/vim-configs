0f4d2058d30032857eb3bbcbe5559b5ce6ac0f76
Vincent Mercator <vmercator@protonmail.com>
2024-10-23 20:43:14 -0400

    Add Quarto-like icon for QMD files

--- plugin/config-neo-tree.lua
+++ plugin/config-neo-tree.lua
@@ -3,6 +3,17 @@ vim.fn["minpac#add"]("nvim-lua/plenary.nvim")
 vim.fn["minpac#add"]("nvim-tree/nvim-web-devicons")
 vim.fn["minpac#add"]("MunifTanjim/nui.nvim")
 
+require("nvim-web-devicons").setup({
+	override_by_extension = {
+		["qmd"] = {
+			icon = "󰐗",
+			name = "Quarto",
+			color = "#75aadb",
+			cterm_color = 75,
+		},
+	},
+})
+
 require("neo-tree").setup({
 	filesystem = {
 		hijack_netrw_behavior = "open_current",
