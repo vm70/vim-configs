b495248998148bd449158c73441792f6f27f3488
Vincent Mercator <vmercator@protonmail.com>
2024-11-23 15:06:42 -0500

    Update LSP configurations

--- lua/plugins/config-lsp.lua
+++ lua/plugins/config-lsp.lua
@@ -1,28 +1,34 @@
 vim.g.coq_settings = { auto_start = "shut-up" }
 
-local default_setup = function(server)
-	require("lspconfig")[server].setup(require("coq").lsp_ensure_capabilities())
-end
-
 local servers = {
 	golangci_lint_ls = {},
 	gopls = {},
 	lua_ls = {},
 	marksman = { filetypes = { "markdown", "markdown.mdx", "markdown.pandoc", "quarto" } },
 	pylsp = {},
+	ruff_lsp = {},
 	taplo = {},
 	vimls = {},
 	ltex = {
 		settings = {
 			ltex = {
+				language = "en-US",
 				disabledRules = {
 					["en-US"] = { "MORFOLOGIK_RULE_EN_US" },
 				},
 			},
 		},
 	},
-	efm = { filetypes = { "vim", "markdown", "lua" } },
+	efm = {
+		init_options = { documentFormatting = true },
+		settings = {},
+		filetypes = { "vim", "markdown", "lua", "quarto" },
+	},
 }
+
+local default_setup = function(server)
+	require("lspconfig")[server].setup(require("coq").lsp_ensure_capabilities(servers[server]))
+end
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
