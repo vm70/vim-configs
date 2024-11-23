f9bb5601826c78eb3bdec78cdc1d90cf86400c52
Vincent Mercator <vmercator@protonmail.com>
2024-11-23 15:06:34 -0500

    Update commands

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,2 +1,6 @@
 -- Edit Vim RC file
-vim.api.nvim_create_user_command("Vimrc", ":edit $MYVIMRC", {bang = true})
+vim.api.nvim_create_user_command("Vimrc", ":edit $MYVIMRC", { bang = true })
+
+-- Quarto Commands
+vim.api.nvim_create_user_command("QuartoPreview", ":terminal quarto preview", { bang = true })
+vim.api.nvim_create_user_command("QuartoRender", ":terminal quarto render", { bang = true })
