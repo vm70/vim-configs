f2969dbdc055ec5002141a13fd82269fb09c9fe2
Vincent Mercator <vmercator@protonmail.com>
2025-02-20 20:18:50 -0500

    Make some language servers optional based on what's installed

--- lua/plugins/config-lsp.lua
+++ lua/plugins/config-lsp.lua
@@ -1,14 +1,9 @@
 vim.g.coq_settings = { auto_start = "shut-up" }
 
 local servers = {
-	golangci_lint_ls = {},
-	gopls = {},
 	lua_ls = {},
 	marksman = { filetypes = { "markdown", "markdown.mdx", "markdown.pandoc", "quarto" } },
-	pylsp = {},
-	ruff_lsp = {},
 	taplo = {},
-	vimls = {},
 	ltex = {
 		settings = {
 			ltex = {
@@ -19,12 +14,21 @@ local servers = {
 			},
 		},
 	},
-	efm = {
-		init_options = { documentFormatting = true },
-		settings = {},
-		filetypes = { "vim", "markdown", "lua", "quarto" },
-	},
 }
+
+if vim.fn.executable("go") == true then
+	servers.golangci_lint_ls = {}
+	servers.gopls = {}
+end
+
+if vim.fn.executable("python") == true then
+	servers.pylsp = {}
+	servers.ruff_lsp = {}
+end
+
+if vim.fn.executable("npm") == true then
+	servers.vimls = {}
+end
 
 local default_setup = function(server)
 	require("lspconfig")[server].setup(require("coq").lsp_ensure_capabilities(servers[server]))
