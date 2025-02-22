93a4019ed62c0cb59dfe898c2c8142bdc4776899
Vincent Mercator <vmercator@protonmail.com>
2025-02-22 09:52:49 -0500

    Simplify Nvim-tree customization

--- lua/plugins/config-nvim-tree.lua
+++ lua/plugins/config-nvim-tree.lua
@@ -8,41 +8,9 @@ local function my_on_attach(bufnr)
 	-- default mappings
 	api.config.mappings.default_on_attach(bufnr)
 
-	local function edit_or_open()
-		local node = api.tree.get_node_under_cursor()
-
-		if node.nodes ~= nil then
-			-- expand or collapse folder
-			api.node.open.edit()
-		else
-			-- open file
-			api.node.open.edit()
-			-- Close the tree if file was opened
-			api.tree.close()
-		end
-	end
-
-	-- open as vsplit on current node
-	local function vsplit_preview()
-		local node = api.tree.get_node_under_cursor()
-
-		if node.nodes ~= nil then
-			-- expand or collapse folder
-			api.node.open.edit()
-		else
-			-- open file as vsplit
-			api.node.open.vertical()
-		end
-
-		-- Finally refocus on tree if it was lost
-		api.tree.focus()
-	end
-
 	-- on_attach
-	vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
-	vim.keymap.set("n", "<CR>", edit_or_open, opts("Edit Or Open"))
-	vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
 	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
 end
 
 require("nvim-tree").setup({
