330ae139152800091d53490777721eb21b43a88b
Vincent Mercator <vmercator@protonmail.com>
2025-02-21 19:40:21 -0500

    Change Nvim tree keybindings

--- lua/plugins/config-nvim-tree.lua
+++ lua/plugins/config-nvim-tree.lua
@@ -42,7 +42,7 @@ local function my_on_attach(bufnr)
 	vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
 	vim.keymap.set("n", "<CR>", edit_or_open, opts("Edit Or Open"))
 	vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
-	vim.keymap.set("n", "h", api.tree.close, opts("Close"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
 end
 
 require("nvim-tree").setup({
