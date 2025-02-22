e6256b72a6990b30ffc84bd2e9bab9585b9a9115
Vincent Mercator <vmercator@protonmail.com>
2025-02-22 09:56:44 -0500

    Add accidental shift-commands

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_user_command("Vimrc", ":edit $MYVIMRC", { bang = true })
 -- Quarto Commands
 vim.api.nvim_create_user_command("QuartoPreview", ":terminal quarto preview", { bang = true })
 vim.api.nvim_create_user_command("QuartoRender", ":terminal quarto render", { bang = true })
+
+-- Accidental shift-commands
+vim.api.nvim_create_user_command("W", ":w", { bang = true })
+vim.api.nvim_create_user_command("WA", ":wa", { bang = true })
+vim.api.nvim_create_user_command("WQ", ":wq", { bang = true })
+vim.api.nvim_create_user_command("WQA", ":wqa", { bang = true })
+vim.api.nvim_create_user_command("WQa", ":wqa", { bang = true })
+vim.api.nvim_create_user_command("Wa", ":wa", { bang = true })
+vim.api.nvim_create_user_command("Wq", ":wq", { bang = true })
+vim.api.nvim_create_user_command("Wqa", ":wqa", { bang = true })
