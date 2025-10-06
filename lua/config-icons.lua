b10f27e64f5895c8e4e3e8d2a6ce0863e607ca4e
Vincent Mercator <vmercator@protonmail.com>
2025-10-05 23:12:55 -0400

    Add `markdown.pandoc` filetype to `mini.icons`

--- lua/config-icons.lua
+++ lua/config-icons.lua
@@ -1,3 +1,7 @@
-require("mini.icons").setup({})
+require("mini.icons").setup({
+	filetype = {
+		["markdown.pandoc"] = { glyph = "󰍔", hl = "MiniIconsGrey" },
+	},
+})
 require("mini.icons").mock_nvim_web_devicons()
 require("mini.icons").tweak_lsp_kind()
