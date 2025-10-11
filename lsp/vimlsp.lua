9dd3438409401034718044d7716e48da0db9f440
Vincent Mercator <vmercator@protonmail.com>
2025-10-11 19:08:35 -0400

    Show full path of Vim LSP

--- lsp/vimlsp.lua
+++ lsp/vimlsp.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-	cmd = { "/usr/bin/npx", "-y", "vim-language-server", "--stdio" },
+	cmd = { vim.fn.expand("$HOME") .. "/.npm/bin/vim-language-server", "--stdio" },
 	filetypes = { "vim" },
 	root_markers = { "vimrc", "init.vim" },
 }
