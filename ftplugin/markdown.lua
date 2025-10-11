339924a821c61d6102f8422819483535b45031c9
Vincent Mercator <vmercator@protonmail.com>
2025-10-11 14:15:36 -0400

    Make markdown commands buffer-only

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -4,8 +4,8 @@ vim.opt_local.spelloptions = ""
 vim.opt_local.wrap = true
 vim.opt_local.conceallevel = 0
 
-vim.keymap.set("i", "<C-BS>", "<C-W>")
-vim.keymap.set("i", "<S-Tab>", "<cmd><<CR>")
+vim.keymap.set("i", "<C-BS>", "<C-W>", { buffer = true })
+vim.keymap.set("i", "<S-Tab>", "<cmd><<CR>", { buffer = true })
 
-vim.keymap.set("v", "<S-Tab>", "<gv")
-vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv", { buffer = true })
+vim.keymap.set("v", "<Tab>", ">gv", { buffer = true })
