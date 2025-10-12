d9de7f91f77ddfba8d23afdba51e18a57bbc02df
Vincent Mercator <vmercator@protonmail.com>
2025-10-12 13:21:53 -0400

    Switch `string.sub` with `vim.fn.fnamemodify`

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ vim.cmd("silent! colorscheme gruvbox")
 local lsp_config_glob = vim.fn.glob(vim.fn.stdpath("config") .. "/lsp/*")
 local lsp_config_list = vim.fn.split(lsp_config_glob, "\n")
 for _, lsp_config_file in ipairs(lsp_config_list) do
-	if string.sub(lsp_config_file, -4) == ".lua" then
+	if vim.fn.fnamemodify(lsp_config_file, ":e") == "lua" then
 		local lsp_name = vim.fn.fnamemodify(lsp_config_file, ":t:r")
 		vim.lsp.enable(lsp_name)
 	end
