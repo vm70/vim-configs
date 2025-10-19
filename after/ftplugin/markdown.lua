ee71aace36ef3552e29593fda6dc3f5cf774b309
Vincent Mercator <vmercator@protonmail.com>
2025-10-19 18:43:38 -0400

    Reorganize `after/ftplugin/markdown.lua`

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,11 +1,13 @@
-vim.g.markdown_folding = 1
-
+vim.opt_local.autoindent = true
+vim.opt_local.breakindent = true
+vim.opt_local.conceallevel = 0
+vim.opt_local.linebreak = true
 vim.opt_local.spelloptions = ""
 vim.opt_local.wrap = true
-vim.opt_local.conceallevel = 0
 
 vim.keymap.set("i", "<C-BS>", "<C-W>", { buffer = true })
 vim.keymap.set("i", "<S-Tab>", "<cmd><<CR>", { buffer = true })
-
 vim.keymap.set("v", "<S-Tab>", "<gv", { buffer = true })
 vim.keymap.set("v", "<Tab>", ">gv", { buffer = true })
+
+vim.g.markdown_folding = 1
