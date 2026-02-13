22ffd81e3453f4eb112de4ddfa88f419c27d3508
Vincent Mercator <vmercator@protonmail.com>
2026-02-12 20:49:25 -0500

    Fix `vim-slime` config

--- init.lua
+++ init.lua
@@ -219,7 +219,13 @@ end)
 
 -- Jupyter / REPL
 later(function()
+	vim.g.slime_target = "neovim"
+	vim.g.slime_no_mappings = false
 	add({ source = "jpalardy/vim-slime" })
+	vim.g.slime_input_pid = false
+	vim.g.slime_suggest_default = true
+	vim.g.slime_menu_config = false
+	vim.g.slime_neovim_ignore_unlisted = false
 end)
 
 -- Sleuth
