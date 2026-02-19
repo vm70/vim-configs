c157c1742abe0dffaaddc5595526034318ab49b1
Vincent Mercator <vmercator@protonmail.com>
2026-02-19 14:35:05 -0500

    Disable more keymaps on Fugitive

--- after/ftplugin/fugitive.lua
+++ after/ftplugin/fugitive.lua
@@ -1,2 +1,5 @@
-vim.b.minisurround_disable = true
+vim.b.miniai_disable = true
+vim.b.minibracketed_disable = true
 vim.b.miniclue_disable = true
+vim.b.minimove_disable = true
+vim.b.minisurround_disable = true
