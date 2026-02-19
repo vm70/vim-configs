4f1acfd447c78376c3165944139b75dbfab8fa1c
Vincent Mercator <vmercator@protonmail.com>
2026-02-19 14:35:20 -0500

    Remove broken Markdown keymaps

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -5,8 +5,6 @@ vim.opt_local.linebreak = true
 vim.opt_local.spelloptions = ""
 vim.opt_local.wrap = true
 
-vim.keymap.set("i", "<C-BS>", "<C-W>", { buffer = true })
-vim.keymap.set("i", "<S-Tab>", "<cmd><<CR>", { buffer = true })
 vim.keymap.set("v", "<S-Tab>", "<gv", { buffer = true })
 vim.keymap.set("v", "<Tab>", ">gv", { buffer = true })
 
