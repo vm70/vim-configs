da63c49d78d2ea7fbe77fe32e5e73340a52bf998
Vincent Mercator <vmercator@protonmail.com>
2026-02-22 13:45:25 -0500

    Revamp keymaps to use defaults

--- plugin/20_keymaps.lua
+++ plugin/20_keymaps.lua
@@ -8,40 +8,38 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Escape Terminal
-vim.keymap.set("t", "<C-w>", "<C-\\><C-N><C-w>", { desc = "Escape Terminal Mode" })
+vim.keymap.set("t", "<C-w>", "<C-\\><C-N><C-w>", { desc = "Escape terminal mode" })
 
 -- Stop highlight search
 vim.keymap.set("n", "<Esc><Esc>", "<cmd>nohlsearch<CR>", { desc = "Stop highlight search" })
-
--- Commenting with Ctrl-/
-vim.keymap.set("n", "<C-_>", "<C-v>gc<CR>k", { desc = "Toggle comment" })
-vim.keymap.set("v", "<C-_>", "gc<CR>k", { desc = "Toggle comment" })
 
 -- File tree
 vim.keymap.set("n", "<leader>E", "<cmd>NvimTreeToggle %:h<CR>", { desc = "Toggle file tree, open at file's parent" })
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
 
--- LSP Commands
-vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename" })
-vim.keymap.set("n", "<leader>cC", vim.lsp.codelens.refresh, { desc = "Refresh Codelens" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-vim.keymap.set("n", "<leader>cc", vim.lsp.codelens.run, { desc = "Run Codelens" })
-vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format Buffer" })
-vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
-vim.keymap.set("n", "<leader>xx", vim.diagnostic.setqflist, { desc = "View Diagnostics in Quickfix List" })
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Go to implementation" })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
-vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+-- LSP keymaps (using global defaults)
+vim.keymap.set({ "n", "v" }, "gra", vim.lsp.buf.code_action, { desc = "Select code action" })
+vim.keymap.set("n", "gri", vim.lsp.buf.implementation, { desc = "List implementations" })
+vim.keymap.set("n", "grn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "grr", vim.lsp.buf.references, { desc = "List all references" })
+vim.keymap.set("n", "grt", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+vim.keymap.set("n", "gO", vim.lsp.buf.document_symbol, { desc = "List all symbols in current buffer" })
+vim.keymap.set("i", "<C-S>", vim.lsp.buf.signature_help, { desc = "Show signature information" })
 
--- CTRL-P for Commands
-vim.keymap.set("n", "<C-P>", "<cmd>FzfLua commands<CR>")
+-- Alternate LSP Keymaps
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol" })
+vim.keymap.set("n", "grD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "grL", vim.lsp.codelens.refresh, { desc = "Refresh code lens" })
+vim.keymap.set("n", "grd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "grf", vim.lsp.buf.format, { desc = "Format buffer" })
+vim.keymap.set("n", "grl", vim.lsp.codelens.run, { desc = "Run code lens" })
+vim.keymap.set("n", "grx", vim.diagnostic.setqflist, { desc = "View diagnostics in quickfix list" })
 
 -- Vim-Slime / Vim-Slime-Cells
-vim.keymap.set("n", "<leader>sc", "<Plug>SlimeSendCell", { desc = "Send cell" })
-vim.keymap.set("n", "<leader>sj", "<Plug>SlimeCellsNext", { desc = "Go to next cell" })
-vim.keymap.set("n", "<leader>sk", "<Plug>SlimeCellsPrev", { desc = "Go to next cell" })
+vim.keymap.set("n", "<leader>sc", "<Plug>SlimeSendCell", { desc = "Send code cell" })
+vim.keymap.set("n", "<leader>sC", "<Plug>SlimeCellsSendAndGoToNext", { desc = "Send code cell & go to next" })
+vim.keymap.set("n", "<leader>sj", "<Plug>SlimeCellsNext", { desc = "Go to next code cell" })
+vim.keymap.set("n", "<leader>sk", "<Plug>SlimeCellsPrev", { desc = "Go to next code cell" })
 
 -- }}}
 -- Moving Visual Regions {{{
