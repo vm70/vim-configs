450f29387ff93a1965d38d91f96710e8bb3e36dc
Vincent Mercator <vmercator@protonmail.com>
2026-02-22 14:47:58 -0500

    Add commands for installing / updating `LanguageServer.jl`

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -1,1 +1,20 @@
-vim.lsp.enable('julials')
+-- Install the Julia LSP server according to nvim-lspconfig.
+local function julia_lsp_install()
+	vim.cmd(
+		[[terminal julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer"); Pkg.add("StaticLint")']]
+	)
+end
+
+-- Update the Julia LSP server according to nvim-lspconfig.
+local function julia_lsp_update()
+	vim.cmd([[terminal julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()']])
+end
+
+vim.api.nvim_create_user_command(
+	"JuliaLspInstall",
+	julia_lsp_install,
+	{ desc = "Install Julia LSP Server", bang = true }
+)
+vim.api.nvim_create_user_command("JuliaLspUpdate", julia_lsp_update, { desc = "Update Julia LSP Server", bang = true })
+
+vim.lsp.enable("julials")
