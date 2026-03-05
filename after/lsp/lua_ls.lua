cd952b37d885b6b9dbc52d3ba17307863caa03cd
Vincent Mercator <vmercator@protonmail.com>
2026-03-04 19:48:22 -0500

    Rearrange LSP configurations

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,5 +1,10 @@
 ---@type vim.lsp.Config
 return {
+	cmd = {
+		"lua-language-server",
+		"--logpath=$HOME/.cache/lua-language-server/log",
+		"--metapath=$HOME/.cache/lua-language-server/meta",
+	},
 	on_attach = function(client, _)
 		client.server_capabilities.documentFormattingProvider = (not vim.fn.executable("stylua"))
 		client.server_capabilities.documentRangeFormattingProvider = (not vim.fn.executable("stylua"))
