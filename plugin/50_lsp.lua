b336ea212710b15d2daf0cb74381c3f3720fd6b8
Vincent Mercator <vmercator@protonmail.com>
2026-03-05 21:21:39 -0500

    Add BASH / SH snippets

--- plugin/50_lsp.lua
+++ plugin/50_lsp.lua
@@ -119,10 +119,12 @@ later(function()
 	-- Define language patterns to work better with 'friendly-snippets'
 	local latex_patterns = { "latex/**/*.json", "**/latex.json" }
 	local lang_patterns = {
+		bash = { "shell/*.json" },
 		cpp = { "c/*.json", "cpp/*.json" },
 		markdown_inline = { "markdown.json" },
 		pandoc = { "pandoc.json", "markdown.json" },
 		plaintex = latex_patterns,
+		sh = { "shell/*.json" },
 		tex = latex_patterns,
 	}
 
