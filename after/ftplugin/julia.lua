fbc8c7a825fa8bb2369fbf86b25a2ea7fc161599
Vincent Mercator <vmercator@protonmail.com>
2026-04-03 21:14:07 -0400

    Use `$HOME` in Julia LSP setup

--- after/ftplugin/julia.lua
+++ after/ftplugin/julia.lua
@@ -1,20 +1,38 @@
 -- Install the Julia LSP server according to nvim-lspconfig.
-local function julia_lsp_install()
+local function lsp_julia_install()
 	vim.cmd(
-		[[terminal julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer"); Pkg.add("StaticLint")']]
+		[[terminal julia --project=$HOME/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.add("LanguageServer"); Pkg.add("SymbolServer"); Pkg.add("StaticLint")']]
 	)
 end
 
 -- Update the Julia LSP server according to nvim-lspconfig.
-local function julia_lsp_update()
-	vim.cmd([[terminal julia --project=~/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()']])
+local function lsp_julia_update()
+	vim.cmd([[terminal julia --project=$HOME/.julia/environments/nvim-lspconfig -e 'using Pkg; Pkg.update()']])
+end
+
+-- Instantiate the Julia LSP server according to nvim-lspconfig.
+local function lsp_julia_instantiate()
+	local abspath = vim.fs.abspath(".")
+	vim.fn.system({
+		"julia",
+		"--project=" .. abspath,
+		"-e",
+		"'using Pkg; Pkg.instantiate()'",
+	})
 end
 
 vim.api.nvim_create_user_command(
-	"JuliaLspInstall",
-	julia_lsp_install,
+	"LspJuliaInstall",
+	lsp_julia_install,
 	{ desc = "Install Julia LSP Server", bang = true }
 )
-vim.api.nvim_create_user_command("JuliaLspUpdate", julia_lsp_update, { desc = "Update Julia LSP Server", bang = true })
+
+vim.api.nvim_create_user_command("LspJuliaUpdate", lsp_julia_update, { desc = "Update Julia LSP Server", bang = true })
+
+vim.api.nvim_create_user_command(
+	"LspJuliaInstantiate",
+	lsp_julia_instantiate,
+	{ desc = "Instantiate Julia LSP Server here", bang = true }
+)
 
 vim.lsp.enable("julials")
