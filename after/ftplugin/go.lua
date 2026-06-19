6ce5537e5a9b66e7051bb4e0cde1f7692554c8bd
Vincent Mercator <vmercator@protonmail.com>
2026-06-19 17:54:34 -0400

    Add golangci-lint language server

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -4,3 +4,11 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 
 vim.lsp.enable("gopls")
+
+-- Installation for `golangci-lint-langserver`:
+--
+-- ```bash
+-- go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
+-- go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+-- ```
+vim.lsp.enable("golangci_lint_ls")
